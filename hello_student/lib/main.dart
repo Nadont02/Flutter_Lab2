36d7d3b627df5a2ce0c5ddb20c200022f19055fd
Никита <dunyushkin2007@mail.ru>
2026-04-28 21:00:20 +0300

    completed task 3 and completed labs

--- hello_student/lib/main.dart
+++ hello_student/lib/main.dart
@@ -4,12 +4,18 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Container(
-        child: Text("Привет! Меня зовут Никита.Я студент группы ИСП-233"     
+      body: Column(
+        children: [
+           Text(
+              "Привет! Меня зовут Никита. Я студент группы ИСП-233"
+              ),
+            Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByaHVrAK_LZz8G12q8kn6S14y0S9-6g_Rog&s',
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
 }
 
