9512014fa86d8cc44c1c6b958bd864a65c39445e
Никита <dunyushkin2007@mail.ru>
2026-04-28 20:44:40 +0300

    completed task 2

---
+++ hello_student/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Container(
+        child: Text("Привет! Меня зовут Никита.Я студент группы ИСП-233"     
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
